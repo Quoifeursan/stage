b2a5f4e280f402ca49788777434412cf0fd61853
Quoifeursan <134583797+Quoifeursan@users.noreply.github.com>
2023-05-30 16:33:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById('toggleSidebar').addEventListener('click', function() {
   document.querySelector('.sidebar').classList.toggle('active');
 });
+
+// Fonction pour afficher la boîte de dialogue
+    function dial() {
+      window.alert("Cette section est indisponible car le stage n'a pas encore démaré ! Cette page sera disponible au début du stage.");
+    }
+    
+    // Attacher un événement de clic aux liens
+    document.addEventListener('DOMContentLoaded', function() {
+      var liens = document.querySelectorAll('nav ul li a');
+      for (var i = 0; i < liens.length; i++) {
+        liens[i].addEventListener('click', dial);
+      }
+    });
